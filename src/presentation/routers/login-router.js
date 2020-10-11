650c4b0b3b21ef6384967faea7d5057c4d3e16ce
diegovinci <diego.vinciguerra@bivee.com.br>
2020-10-11 14:49:06 -0300

    move error 401 to a helper method

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -19,8 +19,7 @@ module.exports = class LoginRouter {
     }
 
     this.authUseCase.auth(email, password)
-    return {
-      statusCode: 401
-    }
+
+    return HttpResponse.unauthorized()
   }
 }
