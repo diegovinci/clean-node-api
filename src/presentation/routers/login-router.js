5dcef6c8b1c5f96adf8f98853754aafb248dc3d9
diegovinci <diego.vinciguerra@bivee.com.br>
2020-10-11 15:06:04 -0300

    ensure LoginRouter returns 500 if an invalid AuthUseCase is provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -6,7 +6,7 @@ module.exports = class LoginRouter {
   }
 
   route (httpRequest) {
-    if (!httpRequest || !httpRequest.body) {
+    if (!httpRequest || !httpRequest.body || !this.authUseCase || !this.authUseCase.auth) {
       return HttpResponse.internalServerError()
     }
 
@@ -19,7 +19,6 @@ module.exports = class LoginRouter {
     }
 
     this.authUseCase.auth(email, password)
-
     return HttpResponse.unauthorized()
   }
 }
