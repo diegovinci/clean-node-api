fda447fa73adb579fc5cf83096bcccc4c7e0a173
diegovinci <diego.vinciguerra@bivee.com.br>
2020-10-24 20:32:26 -0300

    ensure TokenGeneration throws an error if no secret is provided

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,11 +1,16 @@
 const jwt = require('jsonwebtoken')
 
+const MissingParamError = require('../errors/missing-param-error')
+
 class TokenGenerator {
   constructor (secret) {
     this.secret = secret
   }
 
   async generate (id) {
+    if (!this.secret) {
+      throw new MissingParamError('secret')
+    }
     return jwt.sign(id, this.secret)
   }
 }
@@ -33,5 +38,11 @@ describe('Token Generator', () => {
     await sut.generate('any_id')
     expect(jwt.id).toBe('any_id')
     expect(jwt.secret).toBe(sut.secret)
+  })
+
+  test('Should throw an error if no secret is provided', async () => {
+    const sut = new TokenGenerator()
+    const promise = sut.generate('any_id')
+    expect(promise).rejects.toThrow(new MissingParamError('secret'))
   })
 })
