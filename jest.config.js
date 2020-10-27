52e227a022c07c83b055987f3723a01994657b8f
diegovinci <diego.vinciguerra@bivee.com.br>
2020-10-26 22:58:34 -0300

    ensure LoadUserByEmailRepository returns an user if user is found

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
-  testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js']
+  collectCoverageFrom: ['**/src/**/*.js'],
+  preset: '@shelf/jest-mongodb'
 }
