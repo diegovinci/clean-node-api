7b6113f5056366bf8213170a9e3a6a773892b73a
diegovinci <diego.vinciguerra@bivee.com.br>
2020-10-28 15:48:06 -0300

    connect to mongodb before listen for app

--- src/main/index.js
+++ src/main/index.js
@@ -1,3 +1,9 @@
 const app = require('./config/app')
+const MongoHelper = require('../infra/helpers/mongo-helper')
+const env = require('./config/env')
 
-app.listen(5858, () => console.log('Server running'))
+MongoHelper.connect(env.mongoUrl)
+  .then(() => {
+    app.listen(5858, () => console.log('Server running'))
+  })
+  .catch(console.error)
