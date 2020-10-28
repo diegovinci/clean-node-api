76e8af71bd08a6a0c95d0d6c0fb57b04b1116bf9
diegovinci <diego.vinciguerra@bivee.com.br>
2020-10-28 12:24:07 -0300

    add express

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   coverageDirectory: 'coverage',
-  coverageProvider: 'babel',
   collectCoverageFrom: ['**/src/**/*.js'],
   preset: '@shelf/jest-mongodb',
   roots: ['src', '__mocks__']
