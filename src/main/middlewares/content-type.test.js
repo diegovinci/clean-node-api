d0ffb460a05f8f9ff29be76ceabaa339122c1536
diegovinci <diego.vinciguerra@bivee.com.br>
2020-10-28 14:24:25 -0300

    ensure requests return the given content-type when it is defined

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -11,4 +11,16 @@ describe('Content-Type Middleware', () => {
       .get('/test-content-type')
       .expect('content-type', /json/)
   })
+
+  test('Should return xml content type if defined', async () => {
+    app.get('/test-content-type-xml', (req, res) => {
+      res
+        .type('xml')
+        .send()
+    })
+
+    await request(app)
+      .get('/test-content-type-xml')
+      .expect('content-type', /xml/)
+  })
 })
